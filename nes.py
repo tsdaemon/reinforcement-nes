4898550e0690a2a3760f9f0fd38f9bca3ae3e6b0
anatolii stehnii <tsdaemon@gmail.com>
2017-08-03 21:12:51 +0300

    multithread

--- nes.py
+++ nes.py
@@ -4,15 +4,18 @@ from multiprocessing import Pool
 
 
 class NESOptimizer(object):
-    def __init__(self, env, alpha, sigma):
+    def __init__(self, env, alpha, sigma, elite_set):
         self.alpha = alpha
         self.sigma = sigma
+        self.elite_set = elite_set
         self.obs_space = get_space_info(env.observation_space)
         self.action_space = get_space_info(env.action_space)
         self.policy = get_policy(self.obs_space, self.action_space)
 
-    def optimize_parallel(self, envs, n_batches, n_episodes_in_batch, verbose=False, stop_on_std_zero=True):
+    def optimize_parallel(self, envs, n_batches, n_episodes_in_batch, verbose=False):
         """ Performs parallel optimization for given environments """
+        if verbose:
+            self._print_start()
 
         # Prepare weights
         w = np.random.rand(self.obs_space['n'] * self.action_space['n'])
@@ -26,8 +29,6 @@ class NESOptimizer(object):
         for j in range(n_batches):
             # Generates random noise for each parameter
             N = np.random.normal(scale=self.sigma, size=(n_episodes_in_batch, w.shape[0]))
-
-
 
             # Evaluates all sets of parameters in parallel
             episodes_done = 0
@@ -44,7 +45,7 @@ class NESOptimizer(object):
 
             reward_history.append(np.mean(rewards))
 
-            w, stop = self._update_w(rewards, stop_on_std_zero, n_episodes_in_batch, N, w, n_episodes_in_batch)
+            w, stop = self._update_w(rewards, n_episodes_in_batch, N, w, n_episodes_in_batch)
 
             if stop:
                 break
@@ -54,26 +55,32 @@ class NESOptimizer(object):
 
         return w, reward_history
 
-    def optimize(self, env, n_batches, n_episodes_in_batch, verbose=False, stop_on_std_zero=True):
-        """ Performs parallel optimization for given environments """
+    def optimize(self, env, n_batches, n_episodes_in_batch,
+                 verbose=False, render=False):
+        """ Performs optimization for given environment """
+        if verbose:
+            self._print_start()
 
         # Prepare weights
-        w = np.random.rand(self.obs_space['n'] * self.action_space['n'])
+        w = np.zeros(self.obs_space['n'] * self.action_space['n']) # np.random.rand(self.obs_space['n'] * self.action_space['n'])
         # Obtained reward stored to evaluate total performance
         reward_history = []
         rewards = np.zeros(n_episodes_in_batch)
 
         for j in range(n_batches):
             # Generates random noise for each parameter
-            N = np.random.normal(scale=self.sigma, size=(n_episodes_in_batch, w.shape[0]))
+            N = np.random.normal(scale=self.sigma, size=(n_episodes_in_batch-1, w.shape[0]))
+            # Use previous set of parameters as a last set of parameters for this batch to prevent negative changes
+            N = np.vstack((N, np.zeros(w.shape[0])))
 
+            # Evaluate the changes
             for i in range(n_episodes_in_batch):
                 w_try = w + N[i]
-                reward = self._run_episode(env, w_try)
+                reward = self._run_episode(env, w_try, render)
                 rewards[i] = reward
             reward_history.append(np.mean(rewards))
 
-            w, stop = self._update_w(rewards, stop_on_std_zero, N, w, n_episodes_in_batch)
+            w, stop = self._update_w(rewards, N, w, n_episodes_in_batch)
 
             if verbose:
                 print("Batch {}/{}, reward mean {}, reward standard deviation {}".format(j + 1, n_batches, np.mean(rewards), np.std(rewards)))
@@ -83,19 +90,23 @@ class NESOptimizer(object):
 
         return w, reward_history
 
-    def _update_w(self, rewards, stop_on_std_zero, N, w, n_episodes_in_batch):
-        # If no difference in reward, probably optimization reached the minimum
+    def _print_start(self):
+        print("Started oprimization for environment with {} {} observation parameters and {} {} actions"
+              .format(self.obs_space['n'], "discrete" if self.obs_space['discrete'] else "continuous",
+                      self.action_space['n'], "discrete" if self.action_space['discrete'] else "continuous"))
+
+    def _update_w(self, rewards, N, w, n_episodes_in_batch):
         std = np.std(rewards)
         m = np.mean(rewards)
-        if std == 0 and stop_on_std_zero: return w, True
-        elif std == 0: std = 0.001
+        # If no difference in rewards, nothing to change
+        if std == 0: return w
         # Reward transformed to weights
         A = (rewards - m) / std
-        # Finally weights are changed on a vector of weighted sum of
+        # Weights are changed on a vector of weighted sum of
         w += self.alpha / (n_episodes_in_batch * self.sigma) * np.dot(N.T, A)
-        return w, False
+        return w
 
-    def _run_episode(self, env, w):
+    def _run_episode(self, env, w, render=False):
         """ Evaluates single episode for given environment and given parameters w """
         done = False
         observation = env.reset()
@@ -103,6 +114,8 @@ class NESOptimizer(object):
         w = w.reshape(self.obs_space['n'], self.action_space['n'])
         while not done:
             action = self.policy(w, observation)
+            if render:
+                env.render()
             observation, reward, done, _ = env.step(action)
             ep_reward += reward
 
--- train.py
+++ train.py
@@ -2,10 +2,10 @@ import argparse
 from nes import NESOptimizer
 import gym
 import matplotlib.pyplot as plt
-import numpy as np
+import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--n-threads', default=4, type=int,
+parser.add_argument('-t', '--n-threads', default=1, type=int,
                     help="Number of threads to use")
 
 parser.add_argument('-p', '--n-episodes', default=10, type=int,
@@ -14,31 +14,53 @@ parser.add_argument('-p', '--n-episodes', default=10, type=int,
 parser.add_argument('-e', '--env-id', type=str, default="CartPole-v1",
                     help="Environment id")
 
-parser.add_argument('-v', '--verbose', type=bool, default=True,
-                    help="Show or not intermediate results")
-
-parser.add_argument('-d', '--std-zero', type=bool, default=False,
-                    help="Stop or not if reward standard deviation becomes zero")
-
 parser.add_argument('-a', '--alpha', type=float, default=0.1, help="Learning rate")
 
 parser.add_argument('-s', '--sigma', type=float, default=0.1, help="Noise scale")
 
+parser.add_argument('-l', '--elite-set', type=float, default=1.0,
+                    help="Best selection")
+
 parser.add_argument('-b', '--n-batches', type=int, default=100,
                     help="Number of batches")
 
+parser.add_argument('-v', '--verbose', type=bool, default=True,
+                    help="Show or not intermediate results")
+
+parser.add_argument('-r', '--render', type=bool, default=False,
+                    help="Render environment")
+
+parser.add_argument('-k', '--api-key', type=str, default='',
+                    help="API key to upload solution")
+
+parser.add_argument('-f', '--file', type=str, default='',
+                    help="File to save results")
+
 
 def train_parallel(args):
+    if args.render or len(args.api_key) > 0:
+        raise RuntimeError("Can not render or watch during parallel training")
+
     envs = [gym.make(args.env_id) for i in range(args.n_threads)]
-    nes = NESOptimizer(envs[0], args.alpha, args.sigma)
-    w, history = nes.optimize_parallel(envs, args.n_batches, args.n_episodes, args.verbose, args.std_zero)
+    nes = NESOptimizer(envs[0], args.alpha, args.sigma, args.elite_set)
+    w, history = nes.optimize_parallel(envs, args.n_batches, args.n_episodes, args.verbose)
+    for env in envs:
+        env.close()
     return history
 
 
 def train(args):
     env = gym.make(args.env_id)
-    nes = NESOptimizer(env, args.alpha, args.sigma)
-    w, history = nes.optimize(env, args.n_batches, args.n_episodes, args.verbose, args.std_zero)
+    if len(args.api_key) > 0:
+        env = gym.wrappers.Monitor(env, './tmp', force=True)
+
+    nes = NESOptimizer(env, args.alpha, args.sigma, args.elite_set)
+    w, history = nes.optimize(env, args.n_batches, args.n_episodes, args.verbose, args.render)
+    env.close()
+
+    if len(args.api_key) > 0:
+        gym.upload('./tmp', api_key=args.api_key)
+
     return history
 
 
@@ -46,12 +68,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.n_threads == 1:
-        reward_history = train(args)
+        reward_history, w = train(args)
     else:
-        reward_history = train_parallel(args)
+        reward_history, w = train_parallel(args)
 
-    plt.plot(reward_history)
-    plt.show()
+    if len(args.file) == 0:
+        plt.plot(reward_history)
+        plt.show()
+    else:
+        results = {'history':reward_history, 'weights': w}
+        with open(args.file, 'w') as f:
+            json.dump(results, f)
+
 
 
 
