ce5be3a189dd79c3800c5a79c2967bdea13cfe0f
anatolii stehnii <tsdaemon@gmail.com>
2017-08-04 18:26:42 +0300

    tuning

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 from subprocess import run
-import os
+import subprocess
 import seaborn as sns
 import matplotlib.pyplot as plt
 import json
@@ -8,52 +8,69 @@ import numpy as np
 import argparse
 
 
-def evaluation_process(i, args):
-    file_name = '{}{}_{}_{}_{}_{}.json'.format(args['folder'], args['env'], args['alpha'], args['sigma'], args['batches'], i)
+def evaluation_process(i, files, args, verbose):
+    file_name = '{}{}_{}_{}_{}_{}_{}.json'.format(args['folder'],
+                                                  args['env'],
+                                                  args['alpha'],
+                                                  args['sigma'],
+                                                  args['episodes'],
+                                                  args['batches'], i)
     process_parameters = ['python3', 'train.py',
                           '-e', args['env'],
                           '--alpha', str(args['alpha']),
                           '--sigma', str(args['sigma']),
+                          '-p', str(args['episodes']),
                           '-b', str(args['batches']),
                           '-f', file_name]
-    print("Started process {}: {}...".format(i+1, ' '.join(process_parameters)))
-    result = run(process_parameters)
-    print("Finished process {}: {}.".format(i+1, result))
-    return file_name
+
+    if verbose:
+        print("Started process {}: {}...".format(i+1, ' '.join(process_parameters)))
+    run(process_parameters, stdout=None if verbose else subprocess.DEVNULL)
+    if verbose:
+        print("Finished process {}.".format(i+1))
+    files.put(file_name)
 
 
-def generate_data(n_runs, args):
+def generate_data(n_runs, args, verbose=False):
+    # Init processes infrastructure
     processes = []
+    files = Queue()
 
+    # Start all processes and wait for finish
     for i in range(n_runs):
-       p = Process(target=evaluation_process, args=(i, args))
+       p = Process(target=evaluation_process, args=(i, files, args, verbose))
        p.start()
        processes.append(p)
-
     for p in processes:
        p.join()
 
-    print(processes)
+    # Extract return values
+    f = []
+    while not files.empty():
+        f.append(files.get())
+
+    return f
 
 
-def load_data(folder):
+def load_data(files):
     runs = []
-    for file in os.listdir(folder):
-        with open(folder+file, 'r') as f:
+    for file in files:
+        with open(file, 'r') as f:
             run_result = json.load(f)
         runs.append(run_result['history'])
     return np.array(runs)
 
 
 if __name__ == "__main__":
-    args = {'alpha': 0.1,
-            'sigma': 0.1,
-            'batches': 75,
+    args1 = {'alpha': 0.2229,
+            'sigma': 0.2542,
+            'batches': 200,
             'folder': './evaluation/',
             'env': 'Acrobot-v1',
-            'n_runs': 10}
+            'episodes': 18,
+            'n_runs': 10 }
 
-    generate_data(args['n_runs'], args)
-    result = load_data(args['folder'])
+    files1 = generate_data(args['n_runs'], args, True)
+    result1 = load_data(files)
     sns.tsplot(data=result)
     plt.show()
---
+++ tune.py
@@ -0,0 +1,46 @@
+from evaluate import generate_data, load_data
+import numpy as np
+from bayes_opt import BayesianOptimization
+
+
+def movingaverage(values, window):
+    weights = np.repeat(1.0, window)/window
+    sma = np.convolve(values, weights, 'valid')
+    return sma
+
+if __name__ == "__main__":
+    args = {'batches': 150,
+            'folder': './evaluation/',
+            'env': 'Acrobot-v1',
+            'n_runs': 5}
+
+    def evaluate(sigma, alpha, episodes):
+        # Set values of tune parameters
+        args['sigma'] = sigma
+        args['alpha'] = alpha
+        args['episodes'] = int(episodes)
+        # Perform estimation
+        files = generate_data(5, args)
+        result = load_data(files)
+        # Calculate mean values for training history
+        m = np.mean(np.array(result), axis=0)
+        assert m.shape[0] == args['batches']
+        # Find best 100 episodes window
+        window = 100/int(episodes)
+        ma = movingaverage(m, window)
+        return max(ma)
+
+    # Hyperparameters to optimize
+    parameters = {
+        'sigma': [0.01, 0.5],
+        'alpha': [0.01, 0.5],
+        'episodes': [2, 20]
+    }
+
+    opt = BayesianOptimization(evaluate, parameters, verbose=1)
+
+    num_iter = 25
+    init_points = 5
+
+    result = opt.maximize(init_points=init_points, n_iter=num_iter)
+    print(result)
